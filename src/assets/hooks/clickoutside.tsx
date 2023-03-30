33bbb7d33b3d60763f4141e0ff53dee5e2a784a4
alireza <comolestyrusistantaneos@gmail.com>
2023-03-30 10:20:10 +0330

    first commit

---
+++ src/assets/hooks/clickoutside.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+function useOnClickOutside(ref: any, handler: Function) {
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [ref, handler]);
+}
+
+export default useOnClickOutside;
---
+++ src/assets/hooks/queryWord.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { TendPoint } from "../store/slices/word.slice";
+
+const fetchWord = async (endpoint: TendPoint, newWord: string) => {
+  const res = await fetch(
+    `https://api.datamuse.com/words?${endpoint}=${newWord}&max=5&md=d`
+  );
+  return await res.json();
+};
+
+const useQueryWord = (endpoint: TendPoint, headWord: string) => {
+  const { status, data } = useQuery({
+    queryKey: ["headWord", headWord, endpoint],
+    queryFn: async () => {
+      const response = await fetchWord(endpoint, headWord);
+      return response;
+    },
+  });
+  return { status, data };
+};
+
+export default useQueryWord;
