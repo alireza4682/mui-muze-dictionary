617189f031b3367877d4c2c8a316d649db8ec236
alireza <comolestyrusistantaneos@gmail.com>
2023-03-31 16:45:47 +0330

    started modal

--- src/components/oneCard.component.tsx
+++ src/components/oneCard.component.tsx
@@ -5,12 +5,12 @@ import OneWord from "./oneWord.component";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
-import CardAction from "@mui/material/CardActions";
 import CloseIcon from "@mui/icons-material/Close";
 import CardHeader from "@mui/material/CardHeader";
 import IconButton from "@mui/material/IconButton";
 import ModeIcon from "../hooks/modeIconchooser";
 import CardSkeleton from "./cardSkeleton.component";
+import Divider from "@mui/material/Divider";
 
 const OneCard = (card: oneCardType) => {
   const { headWord, mode } = card;
@@ -26,8 +26,9 @@ const OneCard = (card: oneCardType) => {
     <Card
       sx={{
         width: "250px",
+        height: "370px",
         borderRadius: "15px",
-        backgroundColor: "pink",
+        backgroundColor: "#e1bee7",
         margin: "15px",
       }}
       variant="elevation"
@@ -41,7 +42,7 @@ const OneCard = (card: oneCardType) => {
         }
         title={headWord}
       />
-
+      <Divider variant="middle" />
       <CardContent
         sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}
       >
