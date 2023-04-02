c166fa8d4da2342c867c674914064ec675c479b0
alireza <comolestyrusistantaneos@gmail.com>
2023-04-02 22:10:43 +0330

    added mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Form from "./components/form.component";
 import CardContainer from "./components/cardContainer.component";
+import Mode from "./components/mode.component";
 
 function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Form />
       <CardContainer />
+      <Mode />
     </div>
   );
 }
--- src/components/mode.component.tsx
+++ src/components/mode.component.tsx
@@ -1,16 +1,40 @@
-import { TendPoint } from "../store/slices/word.slice";
 import ModeIcon from "../hooks/modeIconchooser";
 import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import { setMode } from "../store/slices/word.slice";
+import { useAppDispatch } from "../store/store";
+
+const actions = [
+  { icon: ModeIcon("ml"), name: "MeansLike", setMode: "ml" },
+  { icon: ModeIcon("sl"), name: "SoundsLike", setMode: "sl" },
+  { icon: ModeIcon("rel_nry"), name: "Rhymes", setMode: "rel_nry" },
+  { icon: ModeIcon("rel_trg"), name: "Trigers", setMode: "rel_trg" },
+];
 
 const Mode = () => {
+  const dispatch = useAppDispatch();
+  const handleClick = (mode: string) => {
+    dispatch(setMode(mode));
+  };
   return (
-    <SpeedDial>
-      <Box>{ModeIcon("ml")}</Box>
-      <Box>{ModeIcon("sl")}</Box>
-      <Box>{ModeIcon("rel_nry")}</Box>
-      <Box>{ModeIcon("rel_trg")}</Box>
-    </SpeedDial>
+    <Box sx={{ height: 320, transform: "translateZ(0px)", flexGrow: 1 }}>
+      <SpeedDial
+        ariaLabel="Mode Selection"
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            onClick={() => handleClick(action.setMode)}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
   );
 };
 export default Mode;
