83d0ef4686f16bf0b54a8173df05e1e17029226c
alireza <comolestyrusistantaneos@gmail.com>
2023-04-03 15:26:56 +0330

    made theme its own componenet

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,8 @@ import "./index.css";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createTheme, ThemeProvider, Theme } from "@mui/material/styles";
-import { purple, green } from "@mui/material/colors";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: green[500],
-    },
-  },
-});
-
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./components/theme.component";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
