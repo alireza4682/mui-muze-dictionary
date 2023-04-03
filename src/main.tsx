dbd57a5e30b24a3ac7c207cca86faee3f071d453
alireza <comolestyrusistantaneos@gmail.com>
2023-04-03 15:16:08 +0330

    made first step of theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,30 @@ import "./index.css";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createTheme, ThemeProvider, Theme } from "@mui/material/styles";
+import { purple, green } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
